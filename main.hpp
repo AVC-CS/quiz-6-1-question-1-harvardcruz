965a1115cb1ae15dd33e5231dc80ff76be24f835
harvardcruz <harvard.cruz8@gmail.com>
2024-03-14 16:42:43 -0700

    :)

--- main.hpp
+++ main.hpp
@@ -1,8 +1,65 @@
+#include <iostream>
+using namespace std;
+
 
 //********************
 // Declare Function Prototypes
 //********************
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
+
+int prime;
 
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getTwoValues(int &begin, int &end){
+    cin >> begin >> end;
+    while(begin > end){
+        cout << "Enter two different values where begin < end " << endl;
+        cin >> begin >> end;
+    }
+}
+
+int getNextPrime(int begin){
+    for (int i = begin++; i <= 100; i++){
+        prime = 0;
+        for(int j = 2; j <= 100; j++){
+            if( i == j ){
+                j++;
+            }
+            if(i % j == 0){
+                prime++;
+            }
+        }
+        if(prime == 0){
+            cout << i << " " << endl;
+
+            i=101;
+        }
+    }
+    return begin;
+}
+
+int getPrevPrime(int end){
+    for (int i = end--; i <= 100; i--){
+        prime = 0;
+        for(int j = 2; j <= 100; j--){
+            if( i == j ){
+                j--;
+            }
+            if(i % j == 0){
+                prime--;
+            }
+        }
+        if(prime == 0){
+            cout << "Previous prime number is: " << i << " " << endl;
+            prime = 1;
+            i=101;
+        }
+    }
+    return end;
+
+}
